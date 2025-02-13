8fa8d62f6f1df43ad2242b7eaad5b4f581380a53
moh4b-z <mohammadsalim10000@gmail.com>
2025-02-13 16:40:36 -0300

    Começo

---
+++ app.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+Objetiv: API  referente ao projeto de controle
+Data: 13/02/2025
+Autor: Mohammmad
+Versão: 1.0
+* Observação:
+**********  Para configura e instalar a API, precisamos das 
+            seguintes bibliotecas:
+                express         npm install express --save 
+
+                cors            npm install cors --save 
+
+                body-parser     npm install body-parser --save   
+**********  Para configura e instalar o acesso ao Banco de dados, 
+            precisamos:
+                prisma          npm install prisma --save (conexão com o BD)
+
+                prisma/client   npm install @prisma/client --save   (Executa scripts no BD)
+************************************************************************/
