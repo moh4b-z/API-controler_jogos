261b526817b7f2f6b2e78f82ccc512f8c5562a34
moh4b-z <mohammadsalim10000@gmail.com>
2025-02-13 17:16:28 -0300

    Alteração no controller, adição das funções

--- controller/jogo/controllerJogo.js
+++ controller/jogo/controllerJogo.js
@@ -4,3 +4,62 @@ Data: 13/02/2025
 Autor: Mohammmad
 Versão: 1.0
 ************************************************************************/
+
+
+async function inserirJogo(jogo) {
+    if(
+        corrigirNotNullVarchar(jogo.nome, 80) ||
+        corrigirNotNullVarchar(jogo.data_lacamento, 10) ||
+        corrigirNotNullVarchar(jogo.versao, 10) ||
+        corrigirVarchar(jogo.tamanho, 10) ||
+        corrigirUndefined(jogo.descricao) ||
+        corrigirVarchar(jogo.foto_capa, 200) ||
+        corrigirVarchar(jogo.link, 200)
+    ){
+
+    }
+    
+}
+async function atualizarJogo(jogo) {
+    
+}
+async function excluirJogo(jogo) {
+    
+}
+async function listarTodosJogo(jogo) {
+    
+}
+async function buscarJogo(jogo) {
+    
+}
+
+function corrigirNotNullVarchar(text, letras){
+    if(text == undefined || text == "" || text == null || text.length > letras){
+        return false
+    }else{
+        return true
+    }
+}
+
+function corrigirVarchar(text, letras){
+    if(text == undefined || text.length > letras){
+        return false
+    }else{
+        return true
+    }
+}
+function corrigirUndefined(text){
+    if(text == undefined){
+        return false
+    }else{
+        return true
+    }
+}
+
+module.exports = {
+    inserirJogo,
+    atualizarJogo,
+    excluirJogo,
+    listarTodosJogo,
+    buscarJogo
+}
