98d08913a202dd7fdf848201ef18457865064cce
MOHAMMAD SALIM SOARES SILVA <24122313@CFP127ALUNO.LOCAL>
2025-02-20 17:15:27 -0300

    Coreções

--- controller/jogo/controllerJogo.js
+++ controller/jogo/controllerJogo.js
@@ -42,6 +42,44 @@ async function buscarJogo(jogo) {
     
 }
 
+
+function teste(jogo){
+    if(
+        corrigirNotNullVarchar(jogo.nome, 80) ||
+        corrigirNotNullVarchar(jogo.data_lacamento, 10) ||
+        corrigirNotNullVarchar(jogo.versao, 10) ||
+        corrigirVarchar(jogo.tamanho, 10) ||
+        corrigirUndefined(jogo.descricao) ||
+        corrigirVarchar(jogo.foto_capa, 200) ||
+        corrigirVarchar(jogo.link, 200)
+    ){
+        console.log(corrigirNotNullVarchar(jogo.nome, 80))
+        console.log(corrigirNotNullVarchar(jogo.data_lacamento, 10))
+        console.log(corrigirNotNullVarchar(jogo.versao, 10))
+        console.log(corrigirVarchar(jogo.tamanho, 10))
+        console.log(corrigirUndefined(jogo.descricao))
+        console.log(corrigirVarchar(jogo.foto_capa, 200))
+        console.log(corrigirVarchar(jogo.link, 200))
+        console.log(jogo)
+        return MENSAGE.ERROR_REQUIRED_FIELDS
+    }else{
+        return ("texto certo")
+    }
+    
+}
+
+console.log(teste({
+    "nome": "Mario Bros",
+    "data_lancamento": "1986-06-03",
+    "versao": "1.0",
+    "tamanho": "500Kb",
+    "descricao": "Jogo legal",
+    "foto_capa": "http://foto.jpg",
+    "link": "http://downloadjogo.zip"
+}))
+
+
+
 function corrigirNotNullVarchar(text, letras){
     if(text == undefined || text == "" || text == null || text.length > letras){
         return true
